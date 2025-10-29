4a4bfb258a95724b2ec0042af31b34b152b2ab35
Talha Rana <mtalharana093@gmail.com>
2025-10-29 15:36:03 +0500

    pydantic baseModel for Users

--- main.py
+++ main.py
@@ -264,8 +264,10 @@ PRODUCTS = [
 #   return {"status" : "OK", "lan" : q}
 
 
+  
+
 @app.get("/user")
-async def user_created(name: str, username: str, email:str | None = None, password: str = Annotated[str, Query(max_length=10)]):
+async def user_created(name: str, username: str, email: str, password: Annotated[str, Query(max_length=10)]):
   return {"message" : "Account created", "your_name": name, "username":username, "your_email": email, "password": password }
 
 
