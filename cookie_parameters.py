3561054e27556134d746b414460b1328f2f839f7
Talha Rana <mtalharana093@gmail.com>
2025-11-03 02:50:54 +0500

    Combining with body Parameters

--- cookie_parameters.py
+++ cookie_parameters.py
@@ -50,22 +50,59 @@ class ProductCookie(BaseModel):
 # -----------------------------
 
 
+# class ProductCookie(BaseModel):
+#   model_config = {"extra": "forbid"}
+#   session_id: str
+#   prefered_category : str | None = None
+#   tracking_id : str | None = None
+  
+  
+# @app.get("/products/recommendations")
+# async def get_recommendations(cookies : Annotated[ProductCookie, Cookie()]):
+#   response = {"session_id" : cookies.session_id}
+  
+#   if cookies.prefered_category:
+#     response["message"] = f"Recommendations for {cookies.prefered_category} products"
+#   else:
+#     response["message"] = f"Default recommedations for session {cookies.session_id}"
+#   if cookies.tracking_id:
+#     response["traacking_id"] = cookies.tracking_id
+#   return response
+
+# -----------------------------
+# Combining with body Parameters
+# -----------------------------
+
 class ProductCookie(BaseModel):
   model_config = {"extra": "forbid"}
-  session_id: str
-  prefered_category : str | None = None
+  session_id: str = Field(title="Session ID", description="User session Identifier")
+  prefered_category : str | None = Field(title="Prefered Category", description="User's prefered product category")
   tracking_id : str | None = None
   
   
-@app.get("/products/recommendations")
-async def get_recommendations(cookies : Annotated[ProductCookie, Cookie()]):
+class PriceFilter(BaseModel):
+  min_price : float = Field(ge=0, title="Minimum Price", description="Minimum price for recommendations")
+  max_price : float | None  = Field(default=None, title="Miximum Price", description="Miximum price for recommendations")
+  
+  
+@app.post("/products/recommendations")
+async def get_recommendations(
+  cookies : Annotated[ProductCookie,
+  Cookie()], 
+  price_filter: Annotated[PriceFilter, Body(embed=True)] ):
+  
   response = {"session_id" : cookies.session_id}
   
   if cookies.prefered_category:
-    response["message"] = f"Recommendations for {cookies.prefered_category} products"
-  else:
-    response["message"] = f"Default recommedations for session {cookies.session_id}"
-  if cookies.tracking_id:
-    response["traacking_id"] = cookies.tracking_id
+    response["category"] = cookies.prefered_category
+  response["price_range"] = {
+      "min_price": price_filter.min_price,
+      "max_price" : price_filter.max_price
+    }
+  
   return response
+
+# curl -X POST -H "Content-Type: application/json" -H "Cookie: session_id=abc123; prefered_category=tech; tracking_id=xyz123" -d {\"price_filter\": {\"min_price\": 1000, \"max_price\": 5000}}" http://127.0.0.1:8000/products/recommendations
+
+
 
