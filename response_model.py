633b5b0593cf8aa8a6620c721fce3581e61288fc
Talha Rana <mtalharana093@gmail.com>
2025-11-05 12:35:13 +0500

    without response model parameters

--- response_model.py
+++ response_model.py
@@ -6,6 +6,17 @@ from typing import List
 app = FastAPI(title="Talha Rana AI Engineer")
 
 
-@app.get("/")
-def get():
-  return "Talha Rana AI Engineer"
+class Product(BaseModel):
+  id : int
+  name : str
+  price : float
+  stock : int | None = None
+  
+class ProductOut(BaseModel):
+  name : str
+  price : float
+
+# # without response model parameters 
+@app.get("/product")
+async def get_product() :
+  return { "id": 2, "name": "mobile", "price": 20000, "stock": 20}
