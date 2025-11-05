8e150a7330320a8a7d8d657ca6b49010c586b114
Talha Rana <mtalharana093@gmail.com>
2025-11-05 11:15:39 +0500

    Return JSON Value in List

--- return_type_annotation.py
+++ return_type_annotation.py
@@ -25,20 +25,20 @@ class Product(BaseModel):
   stock : int | None = None
   
 ## return only JSON Value
-@app.get("/product")
-async def get_product() -> Product:
-  # return "hello world" ## ❌ can't return simple string 
-  # return { "id": 1, "name": "laptop", "price": 20000, "stock": 25}     ## ✅ Only return Product defined basemodel
-  return { "id": 1, "name": "laptop", "price": 20000, "stock": 25}
-
-
-
 # @app.get("/product")
 # async def get_product() -> Product:
 #   # return "hello world" ## ❌ can't return simple string 
 #   # return { "id": 1, "name": "laptop", "price": 20000, "stock": 25}     ## ✅ Only return Product defined basemodel
-#   return [
-#     { "id": 1, "name": "laptop", "price": 20000, "stock": 25},
-#     { "id": 2, "name": "mobile", "price": 20000, "stock": 20},
-#     { "id": 3, "name": "laptop", "price": 20000, "stock": 50}
-#   ]
+#   return { "id": 1, "name": "laptop", "price": 20000, "stock": 25}
+
+
+# # Return JSON Value in List
+@app.get("/product")
+async def get_product() -> list[Product]:
+  # return "hello world" ## ❌ can't return simple string 
+  # return { "id": 1, "name": "laptop", "price": 20000, "stock": 25}     ## ✅ Only return Product defined basemodel
+  return [
+    { "id": 1, "name": "laptop", "price": 20000, "stock": 25},
+    { "id": 2, "name": "mobile", "price": 20000, "stock": 20},
+    { "id": 3, "name": "laptop", "price": 20000, "stock": 50}
+  ]
