353913f71be6ec40f3c6d2dfe90846379bfdaf61
Talha Rana <mtalharana093@gmail.com>
2025-11-06 10:42:16 +0500

    return html form without response_model parameters

--- response_model.py
+++ response_model.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from typing import List, Any, Optional
 
@@ -6,21 +7,37 @@ from typing import List, Any, Optional
 app = FastAPI(title="Talha Rana AI Engineer")
 
 
-# class Product(BaseModel):
-#   id : int
-#   name : str
-#   price : float
-#   stock : int | None = None
+class Product(BaseModel):
+  id : int
+  name : str
+  price : float
+  stock : int | None = None
   
-# class ProductOut(BaseModel):
-#   name : str
-#   price : float
+class ProductOut(BaseModel):
+  name : str
+  price : float
 
-# # without response_model parameters 
+# without response_model parameters 
 # @app.get("/product")
 # async def get_product() :
-#   # return { "id": 2, "name": "mobile", "price": 20000, "stock": 20}
-#   return "hello world"
+#   # return { "id": 2, "name": "mobile", "price": 20000, "stock": 20}  ✅ return
+#   # return "hello world"  ✅ return
+
+@app.get("/user", response_class=HTMLResponse)
+def get_user():
+    return """
+    <html>
+      <body>
+        <h2>Login Form</h2>
+        <form action="/login/" method= "post">
+            <label for="username">username</label><br>
+            <input type="text" id="username" name="username"><br>
+            <label for="password">password</label><br>
+            <input type="password" id="password" name="password"><br>
+            <input type="submit"  value="submit">
+        </form>
+      </body>
+    </html>"""
 
 # with response_model parameters
 # @app.get("/product", response_model=Product)
@@ -70,20 +87,20 @@ class UserIn(BaseUser):
 
 
 
-product_db = {
-  "1" : { "id": 1, "name": "mobile", "price": 20000, "stock": 20, "is_active": True},
-  "2" : { "id": 2, "name": "laptop", "price": 50000, "stock": 20, "is_active": False},
+# product_db = {
+#   "1" : { "id": 1, "name": "mobile", "price": 20000, "stock": 20, "is_active": True},
+#   "2" : { "id": 2, "name": "laptop", "price": 50000, "stock": 20, "is_active": False},
   
-}
+# }
 
-print(type(product_db))
+# print(type(product_db))
 
-class Product(BaseModel):
-  id : int
-  name : str
-  price : float 
-  description : Optional[str] = None
-  tax: float = 15.0  # Default tax rate
+# class Product(BaseModel):
+#   id : int
+#   name : str
+#   price : float 
+#   description : Optional[str] = None
+#   tax: float = 15.0  # Default tax rate
   
 # @app.get("/products/{product_id}", response_model=Product, response_model_exclude_unset=True)
 # async def get_product(product_id: str):
@@ -95,6 +112,6 @@ class Product(BaseModel):
 #   return product_db.get(product_id, {})
 
 ## Excluding Specific Fields with response_model_exclude
-@app.get("/products/{product_id}", response_model=Product, response_model_exclude={"name", "price"})
-async def get_product(product_id: str):
-  return product_db.get(product_id, {})
+# @app.get("/products/{product_id}", response_model=Product, response_model_exclude={"name", "price"})
+# async def get_product(product_id: str):
+#   return product_db.get(product_id, {})
