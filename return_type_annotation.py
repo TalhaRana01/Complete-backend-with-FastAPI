c51e6cfe06218100c2165bf2c83514c7f2300f22
Talha Rana <mtalharana093@gmail.com>
2025-11-06 10:34:06 +0500

    code update

--- return_type_annotation.py
+++ return_type_annotation.py
@@ -41,11 +41,11 @@ app = FastAPI(title="Talha Rana AI Engineer")
 
 ## Return type annotation 
 
-class Product(BaseModel):
-  id: int
-  name : str
-  price : float
-  stock : int | None = None
+# class Product(BaseModel):
+#   id: int
+#   name : str 
+#   price : float
+#   stock : int | None = None
   
 ## return only JSON Value
 # @app.get("/product")
@@ -56,28 +56,25 @@ class Product(BaseModel):
 
 
 # # Return JSON Value in List
-@app.get("/product")
-async def get_product() -> List[Product]:
+# @app.get("/product")
+# async def get_product() -> List[Product]:
   # return "hello world" ## ❌ can't return simple string 
   # return { "id": 1, "name": "laptop", "price": 20000, "stock": 25}     ## ❌ do not return Product defined basemodel
-  return [
-    { "id": 1, "name": "laptop", "price": 20000, "stock": 25},
-    { "id": 2, "name": "mobile", "price": 20000, "stock": 20},
-    { "id": 3, "name": "laptop", "stock": 50, "price": 20000,}
-  ]                                                                       ## ✅ Only return list of product defined basemodel
+  # return [
+  #   { "id": 1, "name": "laptop", "price": 20000, "stock": 25},
+  #   { "id": 2, "name": "mobile", "price": 20000, "stock": 20},
+  #   { "id": 3, "name": "laptop", "stock": 50, "price": 20000,}
+  # ]                                                                       ## ✅ Only return list of product defined basemodel
 
 
 ## without return type annotation 
 # @app.post("/product")
 # async def create_product(product: Product):
-#   # return product   ## ✅ return product basemodel in response body
-#   # return "This is a product" ## ✅ return simple String in response body
-#   return [
-#     { "id": 1, "name": "laptop", "price": 20000, "stock": 25},
-#     { "id": 2, "name": "mobile", "price": 20000, "stock": 20},
-#     { "id": 3, "name": "laptop", "stock": 50, "price": 20000,}
+  # return product   ## ✅ return product basemodel in response body
+  # return "This is a product" ## ✅ return simple String in response body
+ 
     
-  # ]
+  
   
 ## with return type annotation  
 # @app.post("/product")
@@ -92,39 +89,39 @@ async def get_product() -> List[Product]:
 #   # ]
 
 
-# class Product(BaseModel):
-#   name : str
-#   price : float
+class Product(BaseModel):
+  name : str
+  price : float
   
   
   
-# class ProductOut(Product):
-#   id : int
-#   name : str
-#   price : float
-#   stock : int | None = None
-  
-  
-# # Restricted data 
-# # function input is Product id, name, price and stock
-# # function return or output name, and price
-# @app.post("/product")
-# async def get_product(product: ProductOut) -> Product:
-#   return product
-
-# # User Password restricted to return
-# class BaseUser(BaseModel):
-#  username: str
-#  full_name : str | None = None
-  
-# class UserIn(BaseUser):
-  # password : str
-  
+class ProductOut(Product):
+  id : int
+  name : str
+  price : float
+  stock : int | None = None
   
   
 # Restricted data 
 # function input is Product id, name, price and stock
 # function return or output name, and price
+@app.post("/product")
+async def get_product(product: ProductOut) -> Product:
+  return product
+
+# User Password restricted to return
+# class BaseUser(BaseModel):
+#  username: str
+#  full_name : str | None = None
+  
+# class UserIn(BaseUser):
+#   password : str
+  
+  
+  
+# ## Restricted data 
+# ## function input is Product id, name, price and stock
+# ## function return or output name, and price
 # @app.post("/user")
 # async def create_user(user: UserIn) -> BaseUser:
 #   return user
