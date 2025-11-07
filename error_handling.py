e992b46a750d0cd8e8ddadcb9328e712436802b2
Talha Rana <mtalharana093@gmail.com>
2025-11-07 16:46:20 +0500

    raise error

---
+++ error_handling.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+items = {
+  "apple": "A juicy fruit",
+  "banana": "A yellow delight"
+  }
+
+# Import HTTPException from FastAPI
+# Using HTTPException
+
+## Get All Items
+# @app.get("/items")
+# async def get_item():
+#   return items
+
+## Get single item
+@app.get("/items/{item_id}")
+async def get_item(item_id: str):
+  if item_id not in items:
+    raise HTTPException(status_code=404, detail="Item not found")
+  return items[item_id]
